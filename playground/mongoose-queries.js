c19beed0c74bd31ccd70cad9c4fc29373d8e7cb5
morkett <dcorkett@gmail.com>
2017-10-11 17:55:17 +0100

    further tests for .

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,8 +1,8 @@
 const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./../server/db/mongoose');
-const {Todo} = require('./../server/models/todo.model');
-const {User} = require('./../server/models/user.model')
+const {Todo} = require('./../server/models/todo.model.js');
+const {User} = require('./../server/models/user.model.js')
 
 var id = '59de2014cc8f9b9a59783965';
 var userId = '59ddf7f6f0430d2d538e9583';
