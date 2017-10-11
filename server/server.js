c41e1d19a2d70e243eb33ddbc4ed74016673a9a6
morkett <dcorkett@gmail.com>
2017-10-11 17:18:06 +0100

    test

--- server/server.js
+++ server/server.js
@@ -3,56 +3,60 @@ const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo.model');
-var {User} = require('./models/user.model');
+// var {Todo} = require('./models/todo.model');
+// var {User} = require('./models/user.model');
 
 var app = express();
 var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req,res) => {
-  var todo = new Todo({
-    text: req.body.text
-  });
-
-  todo.save()
-    .then( (todo) => {
-      res.send(todo);
-    }, (err) => {
-      res.status(400)
-        .send(err);
-    });
+app.get('/', (req,res) => {
+  res.send('here we are');
 });
 
-app.get('/todos', (req, res) => {
-  Todo.find()
-    .then((todos) => {
-      res.send({todos});
-    }, (err) => {
-      res.status(400)
-        .send(err);
-    });
-});
+// app.post('/todos', (req,res) => {
+//   var todo = new Todo({
+//     text: req.body.text
+//   });
+//
+//   todo.save()
+//     .then( (todo) => {
+//       res.send(todo);
+//     }, (err) => {
+//       res.status(400)
+//         .send(err);
+//     });
+// });
 
-app.get('/todos/:id', (req, res) => {
-  var todoId = req.params.id;
-
-  if(!ObjectID.isValid(todoId)) {
-    res.status(404);
-    res.send();
-  }
-
-  Todo.findById(todoId)
-    .then((todo) => {
-      if(!todo) {
-        return res.status(404).send('Todo not found');
-      }
-      res.send({todo});
-    }).catch((err) => {
-      res.status(400).send();
-    });
-});
+// app.get('/todos', (req, res) => {
+//   Todo.find()
+//     .then((todos) => {
+//       res.send({todos});
+//     }, (err) => {
+//       res.status(400)
+//         .send(err);
+//     });
+// });
+//
+// app.get('/todos/:id', (req, res) => {
+//   var todoId = req.params.id;
+//
+//   if(!ObjectID.isValid(todoId)) {
+//     res.status(404);
+//     res.send();
+//   }
+//
+//   Todo.findById(todoId)
+//     .then((todo) => {
+//       if(!todo) {
+//         return res.status(404).send('Todo not found');
+//       }
+//       res.send({todo});
+//     }).catch((err) => {
+//       res.status(400).send();
+//     });
+// });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
